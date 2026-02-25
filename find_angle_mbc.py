fce6f8ccc06d71a6e5fa2d2626538bd27256f9ca
hasaanmehmood <hasaan.mehmood.9@gmail.com>
2026-02-25 23:59:22 +0500

    CHANGE

--- find_angle_mbc.py
+++ find_angle_mbc.py
@@ -4,4 +4,4 @@ AB = int(input())
 BC = int(input())
 
 angle = math.degrees(math.atan2(AB, BC))   # atan2 is safe and standard
-print(f"{round(angle)}\N{DEGREE SIGN}")
+print(f"{round(angle)}\N{DEGREE SIGN}") #CONSOLE LOG
