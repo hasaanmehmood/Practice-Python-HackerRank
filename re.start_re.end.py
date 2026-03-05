fcfd4c1dee5bb6be20800292a2dd12f1e035004c
hasaanmehmood <hasaan.mehmood.9@gmail.com>
2026-03-05 23:31:39 +0500

    added problem: re.start && re.end

---
+++ re.start_re.end.py
@@ -0,0 +1,23 @@
+import re
+
+def find_start_end(s: str, sub: str):
+    # (?=...) is a lookahead that allows overlapping matches
+    matches = list(re.finditer(rf'(?={re.escape(sub)})', s))
+    if not matches:
+        print((-1, -1))
+        return
+
+    for m in matches:
+        start = m.start()
+        end = start + len(sub) - 1  # HackerRank wants inclusive end
+        print((start, end))
+
+
+# Example
+if __name__ == "__main__":
+    s = "aaadaa"
+    sub = "aa"
+    find_start_end(s, sub)
+    # (0, 1)
+    # (1, 2)
+    # (4, 5)
